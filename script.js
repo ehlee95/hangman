10bcd8c8ba81508808521e1578f82ba2a923529e
ehlee95 <eric95lee@gmail.com>
2022-03-11 12:24:17 -0500

    basic game loop finished, need to make it prettier + iron out a couple edge cases

--- script.js
+++ script.js
@@ -55,25 +55,34 @@ function playGame(word) {
 // plays a round (this function triggered when player submits a letter)
 function playRound(letter) {
    
+    // check if letter has already been guessed. if it has, return
+    if (guesses.indexOf(letter) === -1)
+        guesses.push(letter);
+    else
+        return;
+
     // alters right side letter array to signify letter has been guessed
     let letterGrid = document.querySelector("#letters").innerHTML;
     let newGrid = letterGrid.split(letter.toUpperCase());
     let newHTML = newGrid[0] + "<span class='strikeout'>" + letter.toUpperCase() + "</span>" + newGrid[1];
     document.querySelector("#letters").innerHTML = newHTML;
 
-    guesses.push(letter);
-    round += 1;
-
     // if guess is represented in the word, update string on the left side
     // if guess is not represented, move to next image of hangman
     if (charArray.indexOf(letter) !== -1)
         document.querySelector("#blanks").innerHTML = buildString();
     else {
+        round += 1;
         document.querySelector("#paint").src = "./images/hm" + round + ".png"; 
     }
 
     // display win screen if all chars are guessed
+    if (document.querySelector("#blanks").innerHTML.indexOf("_") === -1)
+        document.querySelector("#winPage").style.display = "block";
+
     // display lose screen if round = 8 and hangman is complete
+    if (round === 8)
+        document.querySelector("#losePage").style.display = "block";
 
 }
 
