53ba2757a334f8c59528967856a1fb592106ce9a
ehlee95 <eric95lee@gmail.com>
2022-03-11 11:13:49 -0500

    added features to cross out guessed letters and iterate hangman illustrations

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-console.log("loaded");
-
-let startPage = document.querySelector(".startPage");
+let startPage = document.querySelector("#startPage");
 let wordInput = document.querySelector("#word");
 startPage.style.display = "block";
 let charArray = [];
 let displayString = "";
 let guesses = [];
+let round = 1;
 
 document.querySelector("#enterWord").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,28 +12,32 @@ document.querySelector("#enterWord").addEventListener("submit", (e) => {
     playGame(wordInput.value);
 });
 
-function buildString(wordArr) {
-   
-    let string = "";
+function buildString() {
+
+    // we need to make a string of charArray.length with spaces between each character
+    // one way to do it would be to make a temporary clone of charArray, then replace each character with a "_" if it's not represented in guesses
     
-    for (let i = 0; i < wordArr.length; i++) {
-        // if wordArr[i] matches a guess, append the letter wordArr[i], if not, append a blank    
-        //    for (let j = 0; j < guesses.length; j++) {
-        //        if (guesses[j] === wordArr[i])
-        //            string += wordArr[i];
-        //    }
-            string += wordArr[i];
-            string += " "
+    let string = "";
+    let clone = Array.from(charArray);
+   
+    // check every letter in the clone
+    for (let i = 0; i < clone.length; i++) {
+        if (guesses.indexOf(clone[i]) === -1)
+            clone[i] = "_";
     }
-    string = string.slice(0, -1);
+
+    for (let i = 0; i < clone.length; i++) {
+        string += clone[i] + " ";
+    }
+    string = string.substring(0, string.length - 1)
     return string;
 }
 
 function playGame(word) {
-
-    charArray = wordInput.value.split("")
-    displayString = buildString(charArray);
-    document.querySelector("#blanks").innerHTML = displayString;
+    
+    // start by splitting secret word into array of characters and displaying on the left side
+    charArray = wordInput.value.split("");
+    document.querySelector("#blanks").innerHTML = buildString();
 
     // addEventListener(submit) to playerInput form, which triggers playRound
     document.querySelector("#playerInput").addEventListener("submit", (e) => {
@@ -49,10 +52,31 @@ function playGame(word) {
         // 2. user hasn't guessed all letters
 }
 
-// cycle to next hangman image
-// cross out guessed letter on letter array
-// build new string with letters revealed, and append to #blanks innerHTML
+// plays a round (this function triggered when player submits a letter)
 function playRound(letter) {
-    console.log(letter);    
+   
+    // alters right side letter array to signify letter has been guessed
+    let letterGrid = document.querySelector("#letters").innerHTML;
+    let newGrid = letterGrid.split(letter.toUpperCase());
+    let newHTML = newGrid[0] + "<span class='strikeout'>" + letter.toUpperCase() + "</span>" + newGrid[1];
+    document.querySelector("#letters").innerHTML = newHTML;
+
     guesses.push(letter);
+    round += 1;
+
+    // if guess is represented in the word, update string on the left side
+    // if guess is not represented, move to next image of hangman
+    if (charArray.indexOf(letter) !== -1)
+        document.querySelector("#blanks").innerHTML = buildString();
+    else {
+        document.querySelector("#paint").src = "./images/hm" + round + ".png"; 
+    }
+
+    // display win screen if all chars are guessed
+    // display lose screen if round = 8 and hangman is complete
+
 }
+
+
+
+
