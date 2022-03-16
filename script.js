241bb50455b1baa3ceaf358e1b01fcfaea26c91e
ehlee95 <eric95lee@gmail.com>
2022-03-16 15:33:42 -0400

    made some small changes

--- script.js
+++ script.js
@@ -81,19 +81,19 @@ function buildString() {
 // plays a round (this function triggered when player submits a letter)
 function playRound(letter) {
 
-    // check if letter has already been guessed. if it has, return
+    // adds new letter to guesses array, returns if letter has already been guessed
     if (guesses.indexOf(letter) === -1)
         guesses.push(letter);
     else
         return;
 
-    // alters right side letter array to signify letter has been guessed
+    // adds strikeout class to the guessed letter on the grid
     let letterGrid = document.querySelector("#letters").innerHTML;
     let newGrid = letterGrid.split(letter.toUpperCase());
     let newHTML = newGrid[0] + "<span class='strikeout'>" + letter.toUpperCase() + "</span>" + newGrid[1];
     document.querySelector("#letters").innerHTML = newHTML;
 
-    // if guess is represented in the word, update string on the left side
+    // if guess is represented in the word, update with buildString
     // if guess is not represented, move to next image of hangman
     if (charArray.indexOf(letter) !== -1)
         document.querySelector("#blanks").innerHTML = buildString();
@@ -106,13 +106,13 @@ function playRound(letter) {
     // display win screen if all chars are guessed
     if (document.querySelector("#blanks").innerHTML.indexOf("_") === -1) {
         document.querySelector("#winPage").style.display = "block";    
-        document.querySelectorAll("#winreplay").focus();
+        document.getElementById("letter").blur();
     }
 
     // display lose screen if round = 8 and hangman is complete
     if (round === 8) {
         document.querySelector("#losePage").style.display = "block";
         document.querySelector("#loseText").innerHTML = `Oh no! You've run out of guesses.<br>The word was <b>"${word}"</b>`;
-        document.querySelectorAll("losereplay").focus(); 
+        document.getElementById("letter").blur();
     }
 }
